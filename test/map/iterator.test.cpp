419f0784af8357cae74bbe3e99565cb293d19a72
abrabant <abrabant@student.42.fr>
2021-10-26 08:27:53 +0200

    Add iterator tests

---
+++ test/map/iterator.test.cpp
@@ -0,0 +1,94 @@
+#include "map_testing.hpp"
+
+int
+test_map_iterator_decrement_end(Tester& tester)
+{
+    map<unsigned, unsigned> m;
+
+    for (unsigned i = 0; i != 10; ++i) {
+        m.insert(FT_CONTAINER::make_pair(i, i));
+    }
+
+    map<unsigned, unsigned>::iterator it = m.end();
+
+    p_assert_eq((--it)->first, 9);
+
+    return 0;
+}
+
+int
+test_map_iterator_decrement_reverse_loop(Tester& tester)
+{
+    map<unsigned, unsigned> m;
+    unsigned baseN = 100000;
+
+    for (unsigned i = 0; i != baseN; ++i) {
+        m.insert(FT_CONTAINER::make_pair(i, i));
+    }
+
+    map<unsigned, unsigned>::iterator it = m.end();
+
+    while (it != m.begin()) {
+        p_assert_eq((--it)->first, --baseN);
+    }
+
+    return 0;
+}
+
+int
+test_map_iterator_prefix_increment(Tester& tester)
+{
+    map<unsigned, unsigned> m;
+    unsigned baseN = 10;
+
+    for (unsigned i = 0; i != baseN; ++i) {
+        m.insert(FT_CONTAINER::make_pair(i, i));
+    }
+
+    size_t i = 1;
+    for (map<unsigned, unsigned>::iterator it = m.begin(); it != m.end();) {
+        if (++it != m.end()) {
+            p_assert_eq(it->first, i++);
+        }
+    }
+
+    return 0;
+}
+
+int
+test_map_iterator_postfix_increment(Tester& tester)
+{
+    map<unsigned, unsigned> m;
+    unsigned baseN = 10;
+
+    for (unsigned i = 0; i != baseN; ++i) {
+        m.insert(FT_CONTAINER::make_pair(i, i));
+    }
+
+    size_t i = 0;
+    for (map<unsigned, unsigned>::iterator it = m.begin(); it != m.end();) {
+        p_assert_eq((it++)->first, i++);
+    }
+
+    return 0;
+}
+
+int
+test_map_iterator_to_const_iterator(Tester& tester)
+{
+    map<unsigned, unsigned> m;
+
+    map<unsigned, unsigned>::const_iterator cit = m.begin();
+
+    assert_expr(cit == m.end());
+    assert_expr(m.end() == cit);
+
+    m.insert(FT_CONTAINER::make_pair(0, 0));
+
+    cit = m.begin();
+
+    assert_expr(cit != m.end());
+    assert_expr(m.end() != cit);
+
+    return 0;
+}
