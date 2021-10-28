4458e2961336d15085fd2e7521f7870e5322a0bf
abrabant <abrabant@student.42.fr>
2021-10-28 15:37:57 +0200

    ft::equal: assumes that ranges are of same size to make it compatible with bidirect it

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -69,30 +69,26 @@ template<typename InputIt1, typename InputIt2>
 bool
 equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
 {
-    InputIt2 last2 = first2 + (last1 - first1);
-
-    while (first1 != last1 && first2 != last2) {
+    while (first1 != last1) {
         if (*first1++ != *first2++) {
             return false;
         }
     }
 
-    return (first1 == last1 && first2 == last2);
+    return true;
 }
 
 template<typename InputIt1, typename InputIt2, typename BinaryPredicate>
 bool
 equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate pred)
 {
-    InputIt2 last2 = first2 + (last1 - first1);
-
-    while (first1 != last1 && first2 != last2) {
+    while (first1 != last1) {
         if (!pred(*first1++, *first2++)) {
             return false;
         }
     }
 
-    return (first1 == last1 && first2 == last2);
+    return true;
 }
 
 // }}}
